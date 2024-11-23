5b5bec60c3ee55c71ca6ed8433150eab43fb4dc9
jorditosa <jordisato88@gmail.com>
2024-11-23 09:50:44 +0100

    added register functionality

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,14 @@
 import axios from 'axios'
+import { User } from '../interfaces/user-interfaces'
+import { ENDPOINT } from '../constants/endpoints'
 
-export const createUser = async (user) => {
+export const createUser = async (user: User) => {
+	// Transofmr user object
+	user.points = 0
+	
 	try {
-		const response = await axios.post(`${import.meta.env.VITE_API_URL}/users`, user)
+		const response = await axios.post(`${import.meta.env.VITE_API_URL}${ENDPOINT.users}`, user)
+		console.log(response)
 		return response.status
 	} catch (error) {
 		console.log(error)
--- src/views/auth/Register.tsx
+++ src/views/auth/Register.tsx
@@ -1,85 +1,25 @@
 import { t } from 'i18next'
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import { CircleLoader } from 'react-spinners'
-import { v4 as uuid } from 'uuid'
-import { STATUS_CODE } from '../../constants/constants'
-import { Regex } from '../../constants/regex'
-import UserModel from '../../models/UserModel'
-import { createUser } from '../../services/userService'
-import { selectUser } from '../../store/users/users.slice'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { createUser } from '../../services/userService';
+
+interface Inputs {
+	"username": string;
+	"email": string;
+	"password": string
+}
 
 const Register = () => {
-	const [email, setEmail] = useState('')
-	const [username, setUsername] = useState('')
-	const [legalAge, setLegalAge] = useState(false)
-	const [isValidEmail, setIsValidEmail] = useState(true)
-	const [isValidCity, setIsValidCity] = useState(true)
-	const [isValidLegalAge, setIsValidLegalAge] = useState(true)
-	const [isLoading, setIsLoading] = useState(false)
-	const [msg, setMsg] = useState('')
-	const navigate = useNavigate()
-	const userLogged = useSelector(selectUser)
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>()
+	const onSubmit: SubmitHandler<Inputs> = (data) => createUser(data)
 
-	useEffect(() => {
-		if (userLogged) {
-			navigate('/dashboard')
-		}
-	}, [])
+	const [isLoading, setIsLoading] = useState<boolean>()
 
-	const handleRegistration = async (e: any) => {
-		e.preventDefault()
-		setIsLoading(true)
-		setMsg('')
-
-		// Validations
-		const isValidEmail = Regex.EMAIL.test(email)
-		const isValidCity = email.length >= 3
-		const isValidLegalAge = legalAge
-
-		if (!isValidEmail) {
-			setIsValidEmail(false)
-			setIsLoading(false)
-			return
-		}
-
-		if (!isValidCity) {
-			setIsValidCity(false)
-			setIsLoading(false)
-			return
-		}
-
-		if (!isValidLegalAge) {
-			setIsValidLegalAge(false)
-			setIsLoading(false)
-			return
-		}
-
-		// Generating the object to be sent to the backend
-		const user = {
-			...UserModel,
-			user_status: uuid(),
-			user_email: email,
-			user_code_validation: uuid().slice(0, 6).replace(/(\d{3})(\d{3})/, '$1-$2'),
-			user_name: username,
-		}
-
-		if (isValidEmail && isValidLegalAge) {
-			try {
-				const res = await createUser(user)
-				if (res === STATUS_CODE.CREATED) {
-					navigate('/')
-				} else {
-					setMsg(t('error_msg_userAlreadyCreated'))
-				}
-				setIsLoading(false)
-
-			} catch (error) {
-				console.log(error)
-			}
-		}
-	}
 
 	return (
 		<section className='container px-4 flex flex-col justify-center items-center'>
@@ -95,7 +35,7 @@ const Register = () => {
 			<form
 				className='w-full mt-6 flex flex-col justify-center gap-4'
 				noValidate
-				onSubmit={e => handleRegistration(e)}
+				onSubmit={handleSubmit(onSubmit)}
 			>
 				<div className="relative">
 					<label htmlFor="username" className="block text-lima font-medium text-lima-700">
@@ -103,40 +43,49 @@ const Register = () => {
 					</label>
 					<input
 						id="username"
-						name="username"
 						type="text"
 						placeholder={t('register_input_name_placeholder')}
-						value={username}
-						onChange={e => setUsername(e.target.value)}
-						className={`${isValidCity ? 'border-lima shadow-mint' : 'border-danger shadow-danger'} mt-1 py-2 pl-4 w-full rounded-md border-lima-200 shadow-sm text-lima bg-blue border-4  placeholder:text-lima`}
+						className={`mt-1 py-2 pl-4 w-full rounded-md border-lima-200 shadow-sm text-lima bg-blue border-4  placeholder:text-lima`}
+						{...register("username")}
 					/>
-					{!isValidCity && <p className="text-danger text-md py-1">{t('register_input_name_error')}</p>}
+					{errors.username && <p className="text-danger text-md py-1">{t('register_input_name_error')}</p>}
 				</div>
-				
+
 				<div className="relative">
-					<label htmlFor="UserEmail" className="block text-lima font-medium text-lima-700">
+					<label htmlFor="email" className="block text-lima font-medium text-lima-700">
 						{t('register_input_email_label')}
 					</label>
 					<input
-						id="UserEmail"
-						name="UserEmail"
+						id="email"
 						type="email"
 						placeholder={t('register_input_email_placeholder')}
-						value={email}
-						onChange={e => setEmail(e.target.value)}
-						className={`${isValidEmail ? 'border-lima shadow-mint' : 'border-danger shadow-danger'} mt-1 py-2 pl-4 w-full rounded-md border-lima-200 shadow-sm text-lima bg-blue border-4  placeholder:text-lima`}
+						className={`mt-1 py-2 pl-4 w-full rounded-md border-lima-200 shadow-sm text-lima bg-blue border-4  placeholder:text-lima`}
+						{...register("email")}
 					/>
-					{!isValidEmail && <p className="text-danger text-md py-1">{t('register_input_email_error')}</p>}
+					{errors.email && <p className="text-danger text-md py-1">{t('register_input_email_error')}</p>}
+				</div>
+
+				<div className="relative">
+					<label htmlFor="password" className="block text-lima font-medium text-lima-700">
+						{t('register_input_password_label')}
+					</label>
+					<input
+						id="password"
+						type="password"
+						placeholder={t('register_input_password_placeholder')}
+						className={`mt-1 py-2 pl-4 w-full rounded-md border-lima-200 shadow-sm text-lima bg-blue border-4  placeholder:text-lima`}
+						{...register("password")}
+					/>
+					{errors.password && <p className="text-danger text-md py-1">{t('register_input_email_error')}</p>}
 				</div>
 
 
-				<div>
+				{/* <div>
 					<div className="flex items-center gap-4">
 						<input
 							id="checkbox"
 							name="checkbox"
 							type="checkbox"
-							onChange={e => setLegalAge(e.target.checked)}
 							className="w-4 h-4 text-lima bg-white border-black rounded focus:ring-lima focus:ring-2 cursor-pointer" />
 						<label
 							htmlFor="checkbox"
@@ -146,7 +95,7 @@ const Register = () => {
 						</label>
 					</div>
 					{!isValidLegalAge && <p className="text-danger text-md py-1">{t('register_input_legalAge_error')}</p>}
-				</div>
+				</div> */}
 
 				<button
 					type="submit"
@@ -158,7 +107,6 @@ const Register = () => {
 							: `${t('register_cta_btn')}`
 					}
 				</button>
-				{msg && <p className="text-danger text-md py-1">{msg}</p>}
 			</form>
 
 		</section>
